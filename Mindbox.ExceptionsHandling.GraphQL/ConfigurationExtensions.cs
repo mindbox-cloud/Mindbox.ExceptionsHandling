09e9be3658477980fd2fc21e71ee7387d8588e2c
Nikolay Andreychuk <andreychuk@mindbox.ru>
2022-08-05 13:06:08 +0400

    Упростил настройку graphQL

--- Mindbox.ExceptionsHandling.GraphQL/ConfigurationExtensions.cs
+++ Mindbox.ExceptionsHandling.GraphQL/ConfigurationExtensions.cs
@@ -1,22 +1,11 @@
-﻿using HotChocolate.Execution.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Mindbox.ExceptionsHandling.GraphQL;
 
 public static class ConfigurationExtensions
 {
-	public static IRequestExecutorBuilder AddExceptionsHandling(this IRequestExecutorBuilder requestBuilder)
-	{
-		requestBuilder
-			.AddErrorFilter(
-				serviceProvider => new GraphQLHandleErrorFilter(
-					serviceProvider.GetApplicationService<IExceptionCategoryMatcher>(),
-					serviceProvider.GetApplicationService<ILoggerFactory>()));
-
-		return requestBuilder;
-	}
-
-	public static IServiceCollection AddGraphQLStatusCodeHandling(this IServiceCollection services) =>
-		services.AddHttpResultSerializer<GraphQLHttpAgnosticResultSerializer>();
+	public static IServiceCollection AddGraphQLErrorHandling(this IServiceCollection services) =>
+		services
+			.AddErrorFilter<GraphQLHandleErrorFilter>()
+			.AddHttpResultSerializer<GraphQLHttpAgnosticResultSerializer>();
 }
