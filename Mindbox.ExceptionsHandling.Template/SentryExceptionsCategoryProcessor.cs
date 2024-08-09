f673bb7747f1e8288fbe7e4a0e5403a83cc23820
Ilshat Mukhamedyarov <mukhamedyarov@mindbox.cloud>
2024-08-09 10:02:19 +0300

    *

--- Mindbox.ExceptionsHandling.Template/SentryExceptionsCategoryProcessor.cs
+++ Mindbox.ExceptionsHandling.Template/SentryExceptionsCategoryProcessor.cs
@@ -4,17 +4,26 @@ using Sentry.Extensibility;
 
 namespace Mindbox.ExceptionsHandling.Template;
 
-public class SentryExceptionsCategoryProcessor(
-	IExceptionCategoryMatcher exceptionCategoryMatcher,
-	IOptions<SentryOptions> sentryOptions)
+public class SentryExceptionsCategoryProcessor
 	: ISentryEventProcessor
 {
+	private readonly IExceptionCategoryMatcher _exceptionCategoryMatcher;
+	private readonly IOptions<SentryOptions> _sentryOptions;
+
+	public SentryExceptionsCategoryProcessor(
+		IExceptionCategoryMatcher exceptionCategoryMatcher,
+		IOptions<SentryOptions> sentryOptions)
+	{
+		_exceptionCategoryMatcher = exceptionCategoryMatcher;
+		_sentryOptions = sentryOptions;
+	}
+
 	public SentryEvent? Process(SentryEvent @event)
 	{
 		if (@event.Exception != null)
 		{
-			var category = exceptionCategoryMatcher.TryGetTopCategory(@event.Exception);
-			if (category != null && category.LogLevel < sentryOptions.Value.MinimumEventLevel) return null;
+			var category = _exceptionCategoryMatcher.TryGetTopCategory(@event.Exception);
+			if (category != null && category.LogLevel < _sentryOptions.Value.MinimumEventLevel) return null;
 		}
 
 		return @event;
