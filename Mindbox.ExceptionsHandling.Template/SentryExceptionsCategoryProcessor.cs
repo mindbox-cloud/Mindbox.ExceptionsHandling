fcb504859c515aaffa0ba62e031707d91824d8b0
Ilshat Mukhamedyarov <mukhamedyarov@mindbox.cloud>
2024-08-09 09:51:21 +0300

    exception handling template

---
+++ Mindbox.ExceptionsHandling.Template/SentryExceptionsCategoryProcessor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Options;
+using Sentry;
+using Sentry.Extensibility;
+
+namespace Mindbox.ExceptionsHandling.Template;
+
+public class SentryExceptionsCategoryProcessor(
+	IExceptionCategoryMatcher exceptionCategoryMatcher,
+	IOptions<SentryOptions> sentryOptions)
+	: ISentryEventProcessor
+{
+	public SentryEvent? Process(SentryEvent @event)
+	{
+		if (@event.Exception != null)
+		{
+			var category = exceptionCategoryMatcher.TryGetTopCategory(@event.Exception);
+			if (category != null && category.LogLevel < sentryOptions.Value.MinimumEventLevel) return null;
+		}
+
+		return @event;
+	}
+}
---
+++ Mindbox.ExceptionsHandling.Template/ServiceCollectionExtensions.cs
@@ -0,0 +1,32 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Mindbox.ExceptionsHandling.Template;
+
+public static class ServiceCollectionExtensions
+{
+	/// <summary>
+	///     Добавляет фильтрацию sentry ивентов по <see cref="IExceptionCategory.LogLevel" />.
+	///     Ивент в sentry не будет уходить,
+	///     если его уровень ниже чем указанный уровень в <see cref="SentryOptions.MinimumEventLevel" />.
+	/// </summary>
+	/// <remarks>
+	///     <see cref="SentryOptions.MinimumEventLevel" /> проставляется в конфигах в
+	///     секции <see cref="SentryOptions.SectionName" />.
+	///     Если в конфигах не проставлен <see cref="SentryOptions.MinimumEventLevel" />,
+	///     то будет использоваться уровень <see cref="LogLevel.Error" />.
+	/// </remarks>
+	public static IServiceCollection AddSentryExceptionsCategoryProcessor(
+		this IServiceCollection services,
+		IConfiguration configuration)
+	{
+		services
+			.AddOptions<SentryOptions>()
+			.Bind(configuration.GetSection(SentryOptions.SectionName));
+
+		services.AddSingleton<SentryExceptionsCategoryProcessor>();
+
+		return services;
+	}
+}
