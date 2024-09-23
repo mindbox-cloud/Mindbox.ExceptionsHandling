00fc0ce75604e714fe331a9b10727f5d1a659b70
Ilshat Mukhamedyarov <mukhamedyarov@mindbox.cloud>
2024-09-23 08:36:57 +0300

    Update SentryOptionsConfiguration.cs

--- Mindbox.ExceptionsHandling.Template/SentryOptionsConfiguration.cs
+++ Mindbox.ExceptionsHandling.Template/SentryOptionsConfiguration.cs
@@ -1,5 +1,4 @@
 using Microsoft.Extensions.Options;
-using Sentry;
 using Sentry.AspNetCore;
 using Sentry.Extensions.Logging;
 using Sentry.NLog;
@@ -17,22 +16,22 @@ internal class SentryOptionsConfiguration : IConfigureOptions<SentryAspNetCoreOp
 		_categoryProcessor = categoryProcessor;
 	}
 
-	public void Configure(SentryAspNetCoreOptions options)
-	{
-		options.AddEventProcessor(_categoryProcessor);
-	}
-
-	public void Configure(SentryLoggingOptions options)
-	{
-		options.AddEventProcessor(_categoryProcessor);
-	}
-
 	public void Configure(SentryNLogOptions options)
 	{
 		Configure("autobugsSentry_wrapped", options);
 	}
 
 	public void Configure(string? name, SentryNLogOptions options)
+	{
+		options.AddEventProcessor(_categoryProcessor);
+	}
+
+	public void Configure(SentryAspNetCoreOptions options)
+	{
+		options.AddEventProcessor(_categoryProcessor);
+	}
+
+	public void Configure(SentryLoggingOptions options)
 	{
 		options.AddEventProcessor(_categoryProcessor);
 	}
